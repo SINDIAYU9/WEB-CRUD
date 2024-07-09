8958d0b211a31d2f6f9eedf965494c5209c904ab
SINDIAYU9 <sindi.122150046@student.itera.ac.id>
2024-07-09 12:46:14 +0700

    index

--- WebCRUD-main/index.php
+++ WebCRUD-main/index.php
@@ -214,7 +214,7 @@ $siswa = query("SELECT * FROM siswa ORDER BY nim DESC");
      gsap.to('.data_siswa', {
           duration: 1,
           delay: 0.6,
-          text: 'Data Siswa :)'
+          text: 'Data Mahasiswa Politeknik Gajah Tunggal'
      })
      gsap.from('.navbar', {
           duration: 1,
