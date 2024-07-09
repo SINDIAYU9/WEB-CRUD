b9a50d46718cb16cecff9e9b5290ac7a4e96b731
Wilson Sitompul <wilsonsitompul573@gmail.com>
2024-07-09 20:20:26 +0700

    Update function.php
    Memisahkan file koneksi dan fix ubah data siswa

--- WebCRUD-main/function.php
+++ WebCRUD-main/function.php
@@ -1,23 +1,20 @@
 <?php
-// Koneksi Database
-$koneksi = mysqli_connect("localhost", "root", "", "Data_siswa");
+include "koneksi.php";
 
-// membuat fungsi query dalam bentuk array
+// Membuat fungsi query dalam bentuk array
 function query($query)
 {
-    // Koneksi database
     global $koneksi;
-
     $result = mysqli_query($koneksi, $query);
 
-    // membuat varibale array
-    $rows = [];
+    if (!$result) {
+        die('Error: ' . mysqli_error($koneksi));
+    }
 
-    // mengambil semua data dalam bentuk array
+    $rows = [];
     while ($row = mysqli_fetch_assoc($result)) {
         $rows[] = $row;
     }
-
     return $rows;
 }
 
@@ -25,7 +22,6 @@ function query($query)
 function tambah($data)
 {
     global $koneksi;
-
     $nim = htmlspecialchars($data['nim']);
     $nama = htmlspecialchars($data['nama']);
     $tmpt_Lahir = htmlspecialchars($data['tmpt_Lahir']);
@@ -41,7 +37,6 @@ function tambah($data)
     }
 
     $sql = "INSERT INTO siswa VALUES ('$nim','$nama','$tmpt_Lahir','$tgl_Lahir','$jekel','$jurusan','$email','$gambar','$alamat')";
-
     mysqli_query($koneksi, $sql);
 
     return mysqli_affected_rows($koneksi);
@@ -51,8 +46,7 @@ function tambah($data)
 function hapus($nim)
 {
     global $koneksi;
-
-    mysqli_query($koneksi, "DELETE FROM siswa WHERE nim = $nim");
+    mysqli_query($koneksi, "DELETE FROM siswa WHERE nim = '$nim'");
     return mysqli_affected_rows($koneksi);
 }
 
@@ -60,7 +54,6 @@ function hapus($nim)
 function ubah($data)
 {
     global $koneksi;
-
     $nim = $data['nim'];
     $nama = htmlspecialchars($data['nama']);
     $tmpt_Lahir = htmlspecialchars($data['tmpt_Lahir']);
@@ -69,7 +62,6 @@ function ubah($data)
     $jurusan = $data['jurusan'];
     $email = htmlspecialchars($data['email']);
     $alamat = htmlspecialchars($data['alamat']);
-
     $gambarLama = $data['gambarLama'];
 
     if ($_FILES['gambar']['error'] === 4) {
@@ -78,8 +70,7 @@ function ubah($data)
         $gambar = upload();
     }
 
-    $sql = "UPDATE siswa SET nama = '$nama', tmpt_Lahir = '$tmpt_Lahir', tgl_Lahir = '$tgl_Lahir', jekel = '$jekel', jurusan = '$jurusan', email = '$email', gambar = '$gambar', alamat = '$alamat' WHERE nim = $nim";
-
+    $sql = "UPDATE siswa SET nama = '$nama', tmpt_Lahir = '$tmpt_Lahir', tgl_Lahir = '$tgl_Lahir', jekel = '$jekel', jurusan = '$jurusan', email = '$email', gambar = '$gambar', alamat = '$alamat' WHERE nim = '$nim'";
     mysqli_query($koneksi, $sql);
 
     return mysqli_affected_rows($koneksi);
@@ -88,79 +79,61 @@ function ubah($data)
 // Membuat fungsi upload gambar
 function upload()
 {
-    // Syarat
     $namaFile = $_FILES['gambar']['name'];
     $ukuranFile = $_FILES['gambar']['size'];
     $error = $_FILES['gambar']['error'];
     $tmpName = $_FILES['gambar']['tmp_name'];
 
-    // Jika tidak mengupload gambar atau tidak memenuhi persyaratan diatas maka akan menampilkan alert dibawah
     if ($error === 4) {
         echo "<script>alert('Pilih gambar terlebih dahulu!');</script>";
         return false;
     }
 
-    // format atau ekstensi yang diperbolehkan untuk upload gambar adalah
     $extValid = ['jpg', 'jpeg', 'png'];
     $ext = explode('.', $namaFile);
     $ext = strtolower(end($ext));
 
-    // Jika format atau ekstensi bukan gambar maka akan menampilkan alert dibawah
     if (!in_array($ext, $extValid)) {
         echo "<script>alert('Yang anda upload bukanlah gambar!');</script>";
         return false;
     }
 
-    // Jika ukuran gambar lebih dari 3.000.000 byte maka akan menampilkan alert dibawah
     if ($ukuranFile > 3000000) {
         echo "<script>alert('Ukuran gambar anda terlalu besar!');</script>";
         return false;
     }
 
-    // nama gambar akan berubah angka acak/unik jika sudah berhasil tersimpan
     $namaFileBaru = uniqid();
     $namaFileBaru .= '.';
     $namaFileBaru .= $ext;
 
-    // memindahkan file ke dalam folde img dengan nama baru
     move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
 
     return $namaFileBaru;
 }
 
+// Membuat fungsi registrasi
 function registrasi($data)
 {
     global $koneksi;
-
     $username = strtolower(stripslashes($data["username"]));
     $password = mysqli_real_escape_string($koneksi, $data["password"]);
     $password2 = mysqli_real_escape_string($koneksi, $data["password2"]);
 
-    // cek username sudah ada atau belum
     $result = mysqli_query($koneksi, "SELECT username FROM user WHERE username = '$username'");
-
     if (mysqli_fetch_assoc($result)) {
-        echo "<script>
-                alert('username sudah terdaftar');
-        </script>";
+        echo "<script>alert('Username sudah terdaftar');</script>";
         return false;
     }
-
-    // cek konfirmasi password
 
     if ($password !== $password2) {
-        echo "<script>
-                alert('konfirmasi password tidak sesuai');
-        </script>";
+        echo "<script>alert('Konfirmasi password tidak sesuai');</script>";
         return false;
     }
 
-    // enkripsi password
     $password = password_hash($password, PASSWORD_DEFAULT);
-
-    // tambahkan user baru ke database
-    mysqli_query($koneksi, "INSERT INTO user VALUES('', '$username', '$password')
-    ");
+    mysqli_query($koneksi, "INSERT INTO user VALUES('', '$username', '$password')");
 
     return mysqli_affected_rows($koneksi);
 }
+?>
