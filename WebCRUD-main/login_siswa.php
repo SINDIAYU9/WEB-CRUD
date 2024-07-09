9e104909291f61bdeba35ad5df5ad935c8d55dd7
Wilson Sitompul <wilsonsitompul573@gmail.com>
2024-07-09 21:58:55 +0700

    Update login_siswa.php
    fix login login berdasarkan nama dan nim

--- WebCRUD-main/login_siswa.php
+++ WebCRUD-main/login_siswa.php
@@ -8,12 +8,11 @@ if (isset($_SESSION['siswa_login'])) {
 
 // Memanggil atau membutuhkan file function.php
 require 'function.php';
-include "koneksi.php";
 
 // Jika tombol login diklik
 if (isset($_POST['login'])) {
+    $nama = $_POST['nama'];
     $nim = $_POST['nim'];
-    $password = md5($_POST['password']); // Gunakan md5 untuk password
 
     // Mengambil data siswa berdasarkan nim
     $result = mysqli_query($koneksi, "SELECT * FROM siswa WHERE nim = '$nim'");
@@ -32,65 +31,57 @@ if (isset($_POST['login'])) {
         $error = true;
     }
 }
-
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <!-- Font Google -->
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
-    <!-- My CSS -->
-    <link rel="stylesheet" href="css/login.css">
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- Bootstrap -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+     <!-- Font Google -->
+     <link rel="preconnect" href="https://fonts.gstatic.com">
+     <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+     <!-- My CSS -->
+     <link rel="stylesheet" href="css/login.css">
 
-    <title>Form Login Siswa</title>
+     <title>Login Siswa</title>
 </head>
 
 <body background="img/bg/bck.png">
 
-    <div class="container">
-        <div class="row my-5">
-            <div class="col-md-6 text-center login bg-dark">
-                <h4 class="fw-bold" style="color: white;">Login Siswa</h4>
-                <!-- Pesan error jika login gagal -->
-                <?php if (isset($error)) : ?>
-                <div class="alert alert-danger" role="alert">
-                    NIM atau Password salah!
-                </div>
-                <?php endif; ?>
-                <form action="" method="post">
-                    <div class="form-group user">
-                        <input type="text" class="form-control w-75 mx-auto" placeholder="Masukkan NIM"
-                            name="nim" autocomplete="off" required>
-                    </div>
-                    <div class="form-group my-4">
-                        <input type="password" class="form-control w-75 mx-auto" placeholder="Masukkan Password"
-                            name="password" autocomplete="off" required>
-                    </div>
-                    <div class="d-grid gap-2">
-                        <button class="btn btn-primary btn-lg text-uppercase" type="submit" name="login">Login</button>
-                    </div>
-                    <div class="text-center mt-3">
-                        <a href="registrasi_siswa.php" class="btn btn-danger text-uppercase"><i
-                                class="bi bi-pencil-square"></i> Sign Up</a>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
+     <div class="container">
+          <div class="row my-5">
+               <div class="col-md-6 text-center login bg-dark">
+                    <h4 class="fw-bold" style="color: white;">Login Siswa</h4>
+                    <!-- Ini Error jika tidak bisa login -->
+                    <?php if (isset($error)) : ?>
+                    <?php echo '<script>alert("Nama atau NIM Salah!");</script>'; ?>
+                    <?php endif; ?>
+                    <form action="" method="post">
+                         <div class="form-group user">
+                              <input type="text" class="form-control w-50" placeholder="Masukkan Nama" name="nama" autocomplete="off" required>
+                         </div>
+                         <div class="form-group my-5">
+                              <input type="text" class="form-control w-50" placeholder="Masukkan NIM" name="nim" autocomplete="off" required>
+                         </div>
+                         <button class="btn btn-primary text-uppercase" type="submit" name="login">Login</button>
+                    </form>
+                    <br>
+                    <a href="login.php" class="btn btn-secondary text-uppercase">Login sebagai Admin</a>
+               </div>
+          </div>
+     </div>
 
-    <!-- Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
+     <!-- Bootstrap -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+     </script>
 </body>
 
 </html>
